1d9012e54f3af7cbc79a69b81f1902ae956741bb
Rozales <Rozalesamiral@gmail.com>
2022-11-25 13:31:24 +0000

    test

--- proto/all_test.go
+++ proto/all_test.go
@@ -2,7 +2,7 @@
 //
 // Copyright 2010 The Go Authors.  All rights reserved.
 // https://github.com/golang/protobuf
-//
+// 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
